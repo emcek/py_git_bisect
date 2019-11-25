57ad9ab4b1bc28fdd60f3cda6940ee1b347dc817
Michal Plichta <michal.plichta@nokia.com>
2019-11-25 17:02:08 +0100

    change exception to correct one

--- hello.py
+++ hello.py
@@ -5,6 +5,6 @@ def greet(first_name):
     :type first_name: str
     """
     if not first_name:
-        raise AttributeError
+        raise ValueError
 
     return f'Hello my dear {first_name}!'
