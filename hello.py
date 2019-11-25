7a45c4b389e342d9d32abfebf10aeccb71d8346b
Michal Plichta <michal.plichta@nokia.com>
2019-11-25 17:13:07 +0100

    change raised exception

--- hello.py
+++ hello.py
@@ -1,13 +1,11 @@
-def greet(first_name: str) -> str:
+def greet(first_name: str):
     """
     Simple function which can greet user by name.
 
     :param first_name: name of user
     :type first_name: str
     :raise ValueError
-    :return: greeting string
-    :rtype: str
     """
     if not first_name:
-        raise ValueError
+        raise AttributeError
     return f'Hello my dear {first_name}!'
