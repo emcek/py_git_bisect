77daffb537db0aab367894e6ed6ab71ab9096140
Michal Plichta <michal.plichta@nokia.com>
2019-11-25 17:04:40 +0100

    add return to docstring

--- hello.py
+++ hello.py
@@ -5,6 +5,7 @@ def greet(first_name: str) -> str:
     :param first_name: name of user
     :type first_name: str
     :raise ValueError
+    :return: greeting string
     """
     if not first_name:
         raise ValueError
