69bea1b4ef298928bf0016a054f8f1d6884f866b
Michal Plichta <michal.plichta@nokia.com>
2019-11-25 17:01:24 +0100

    typo

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 def greet(first_name):
     """
-    Simnple finction which can greet user by name.
+    Simple function which can greet user by name.
     :param first_name: name of user
     :type first_name: str
     """
